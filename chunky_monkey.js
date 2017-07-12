8f8a2dbd88379c249fbdea3f4b951ce3d323bece
Narin Sundarabhaya <nrs710@gmail.com>
2017-07-11 22:40:03 -0700

    solved chunky_monkey.js

--- chunky_monkey.js
+++ chunky_monkey.js
@@ -4,17 +4,18 @@ Write a function that splits an array (first argument) into groups the length of
 
 */
 
-
 function chunkArrayInGroups(arr, size) {
-    var newArr = arr.split(",");
+    let newArr = [];
+    let secondArr = 0;
+    let thirdArr = size;
+    for (var i = 0; i < arr.length; i += size) {
+        newArr.push(arr.slice(secondArr, thirdArr));
+        secondArr += size;
+        thirdArr += size;
+    }
     console.log(newArr);
-
-    // return arr;
+    return newArr;
 }
-
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
-
-
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2) // should return [["a", "b"], ["c", "d"]].
 chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) // should return [[0, 1, 2], [3, 4, 5]].
