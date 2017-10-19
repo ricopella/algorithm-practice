3edc6699d92b9bc4f8e93a2f5ea0c09d7b6b8bac
Narin Sundarabhaya <nrs710@gmail.com>
2017-10-19 11:14:07 -0700

    finished even-last & added test

--- even-last.js
+++ even-last.js
@@ -1,34 +1,34 @@
 "use strict";
-var newArray = [];
-var totalSum = 0;
-var multi = 0;
 
 function evenLast(data) {
+    let totalSum = 0;
 
-    if (data === undefined) {
-        newArray = 0;
-        return newArray;
+    if (data.length < 1) {
+        return 0;
 
     } else if (data.length > 1) {
         for (var i = 0; i < data.length; i += 2) {
-            newArray.push(data[i]);
             totalSum += data[i];
-            multi = totalSum * newArray.pop();
-            return multi;
         }
+        return data[data.length - 1] * totalSum
 
     } else if (data.length == 1) {
-        multi = data[0] * data[0];
-        return multi;
+        return data[0] * data[0];
     }
-    // return 0;
 
 };
 
 var assert = require('assert');
 
 if (!global.is_checking) {
-    assert.equal(evenLast([0, 1, 2, 3, 4, 5]), 30, "(0+2+4)*5=30");
+    assert.equal(evenLast([
+        0,
+        1,
+        2,
+        3,
+        4,
+        5
+    ]), 30, "(0+2+4)*5=30");
     assert.equal(evenLast([1, 3, 5]), 30, "(1+5)*5=30");
     assert.equal(evenLast([6]), 36, "(6)*6=36");
     assert.equal(evenLast([]), 0, "An empty array = 0");
--- test/test.js
+++ test/test.js
@@ -2,24 +2,54 @@ const should = require("chai").should();
 const expect = require("chai").expect();
 const piglat = require("../pig-latin");
 const bubble = require("../bubble-sort");
-
+const eLast = require("../even-last");
 
 describe("translatePigLatin", () => {
     it("Should take the first letter and put at the end with 'ay' following", () => {
-        piglat("Myabear").should.equal("abearMyay");
+        piglat("Myabear")
+            .should
+            .equal("abearMyay");
     })
     it("Should take the two letters and put at the end", () => {
-        piglat("pgiot").should.equal("iotpgay");
+        piglat("pgiot")
+            .should
+            .equal("iotpgay");
     })
     it("Should take the first 4 letters and put at thend", () => {
-        piglat("zzza").should.equal("azzzay");
+        piglat("zzza")
+            .should
+            .equal("azzzay");
     })
-
 
 })
 
-describe("bubblesort", () => {
-    it("Should sort all the letters accordingly", () => {
-        bubble([12, 900, 3, 68, 55, 1]).expect.to.not.equal([12, 900, 3, 68, 55, 1]);
+// EVEN LAST
+describe("evenLast", () => {
+    it("Should return zero", () => {
+        eLast([])
+            .should
+            .equal(0);
+    })
+    it("Should return 30", () => {
+        eLast([
+                0,
+                1,
+                2,
+                3,
+                4,
+                5
+            ])
+            .should
+            .equal(30)
+    })
+    it("Should return 30", () => {
+        eLast([1, 3, 5])
+            .should
+            .equal(30)
+    })
+    it("Should return 30", () => {
+        eLast([6])
+            .should
+            .equal(36)
     })
 })
