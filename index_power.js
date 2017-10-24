7b8463b904a9a5574e5d99658692f98b48e0e596
Narin Sundarabhaya <nrs710@gmail.com>
2017-10-24 13:44:25 -0700

    completed index power

---
+++ index_power.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/*
+
+You are given an array with positive numbers and a number N. You should find the N-th power of the element in the array with the index N.
+If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+
+Let's look at a few examples:
+
+    - array = [1, 2, 3, 4] and N = 2, then the result is 32 == 9;
+    - array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+
+*/
+
+function indexPower(array, n) {
+
+    if (array.length > n) {
+        console.log(array[n] ** n);
+        return array[n] ** n;
+    } else {
+        console.log("nope");
+        return -1
+    }
+}
+
+indexPower([
+    96, 92, 94
+], 3);
