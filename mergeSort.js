6bf2d95cbd66db8da2aa7707caba1d33d0ffd060
Narin Sundarabhaya <nrs710@gmail.com>
2020-04-17 11:27:33 -0700

    refactor(mergeSort): reduce loops

--- mergeSort.js
+++ mergeSort.js
@@ -27,15 +27,7 @@ function stitch(left, right) {
         }
     }
 
-    while (left.length) {
-        results.push(left.shift())
-    }
-
-    while (right.length) {
-        results.push(right.shift())
-    }
-
-    return results
+    return [...results, ...left, ...right]
 }
 
 mergeSort([10, 5, 3, 8, 2, 6, 4, 7, 9, 1]); // [1,2,3,4,5,6,7,8,9,10]
