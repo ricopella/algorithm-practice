d249a0339f6a01988cdca5b343b5dfde85d37e08
Narin Sundarabhaya <nrs710@gmail.com>
2023-10-11 17:15:28 +0700

    add two medium problems from today

---
+++ promiseTimelimit.ts
@@ -0,0 +1,36 @@
+/*
+
+Given an asynchronous function fn and a time t in milliseconds, return a new time limited version of the input function. fn takes arguments provided to the time limited function.
+
+The time limited function should follow these rules:
+
+If the fn completes within the time limit of t milliseconds, the time limited function should resolve with the result.
+If the execution of the fn exceeds the time limit, the time limited function should reject with the string "Time Limit Exceeded".
+
+*/
+
+type Fn = (...params: any[]) => Promise<any>;
+
+function timeLimit(fn: Fn, t: number): Fn {
+  return async function (...args) {
+    return new Promise((resolve, reject) => {
+      const timeout = setTimeout(() => {
+        reject('Time Limit Exceeded');
+      }, t);
+      return fn(...args)
+        .then((res) => {
+          clearTimeout(timeout);
+          resolve(res);
+        })
+        .catch((err) => {
+          clearTimeout(timeout);
+          reject(err);
+        });
+    });
+  };
+}
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
